3b0f619e48d89822cc4308a84c7e5b8e1ccc8afc
leo hao <leo hao@DESKTOP-90NIU16>
2019-10-14 09:58:37 +0800

    remove check

--- zdy.py
+++ zdy.py
@@ -33,7 +33,8 @@ logger.addHandler(ch)
 
 # Squid的配置文件语法
 # 将请求转发到父代理
-PEER_CONF = "cache_peer %s parent %s 0 no-query weighted-round-robin weight=1 connect-fail-limit=2 allow-miss max-conn=5\n"
+# PEER_CONF = "cache_peer %s parent %s 0 no-query weighted-round-robin weight=1 connect-fail-limit=2 allow-miss max-conn=5\n"
+PEER_CONF = "cache_peer %s parent %s 0 proxy-only no-query no-digest round-robin connect-fail-limit=10 connect-timeout=15 max-conn=10 name=proxyip-%s\n"
 
 # 可用代理
 GOOD_PROXIES = []
@@ -48,7 +49,7 @@ def check_proxy(proxy):
     ip, port = proxy
     _proxies = {"http": "{}:{}".format(ip, port)}
     try:
-        ip_url = "http://2019.ip138.com/ic.asp"
+        ip_url = "http://httpbin.org/ip"
         res = requests.get(ip_url, proxies=_proxies, timeout=10)
         assert ip in res.content
         logger.info("[GOOD] - {}:{}".format(ip, port))
@@ -61,8 +62,10 @@ def update_conf():
     with open("/etc/squid/squid.conf.original", "r") as F:
         squid_conf = F.readlines()
     squid_conf.append("\n# Cache peer config\n")
+    i = 0
     for proxy in GOOD_PROXIES:
-        squid_conf.append(PEER_CONF % (proxy[0], proxy[1]))
+        i = i + 1
+        squid_conf.append(PEER_CONF % (proxy[0], proxy[1], i))
     with open("/etc/squid/squid.conf", "w") as F:
         F.writelines(squid_conf)
 
@@ -71,7 +74,7 @@ def get_proxy():
     global GOOD_PROXIES
     GOOD_PROXIES = []
     # 1. 获取代理IP资源
-    api_url = "http://s.zdaye.com/?api=YOUR_API&count=100&fitter=1&px=2"
+    api_url = "http://www.zdopen.com/PrivateProxy/GetIP/?api=201910131255595961&akey=8b831758f29758d6&count=1000&fitter=2&order=2&type=1"
     res = requests.get(api_url).content
     if len(res) == 0:
         logger.error("no data")
@@ -82,8 +85,9 @@ def get_proxy():
         proxies = []
         for line in res.split():
             proxies.append(line.strip().split(":"))
-        pool.map(check_proxy, proxies)
-        pool.join()
+            GOOD_PROXIES.append(line.strip().split(":"))
+        # pool.map(check_proxy, proxies)
+        # pool.join()
         # 2. 写入Squid配置文件
         update_conf()
         # 3. 重新加载配置文件
@@ -95,7 +99,7 @@ def main():
     start = time.time()
     while True:
         # 每30秒获取一批新IP
-        if time.time() - start >= 30:
+        if time.time() - start >= 90:
             get_proxy()
             start = time.time()
         time.sleep(5)
